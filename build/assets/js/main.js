a42daf7df773643e041cce59b83784ba773bccee
safarovsafar <safarov7safar@gmail.com>
2019-05-18 01:25:55 +0500

    shot

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,13 +1,3 @@
-const toggler = document.querySelector(".menu__toggler");
-const menu = document.querySelector(".menu");
-const close = document.querySelector('.wrapper');
-
-toggler.addEventListener("click", () => {
-    toggler.classList.toggle("active");
-    menu.classList.toggle("active");
-    close.classList.toggle(toggler);
-});
-
 const myLazyLoad = new LazyLoad({
     elements_selector: ".lazyload"
 });
@@ -17,9 +7,42 @@ AOS.init({
     duration: 2500
 });
 
+// Get all "navbar-burger" elements
+const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
+// Check if there are any navbar burgers
+if ($navbarBurgers.length > 0) {
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+}
 let scroller = new SmoothScroll({
     target: document.querySelector("#scroll-container"), // element container to scroll
     scrollEase: 0.05,
 });
 const scroll = new SmoothScroll('a[href*="#"]');
+
+
+const toggler = document.querySelector('.menu__toggler');
+const menu    = document.querySelector('.menu');
+
+/*
+ * Toggles on and off the 'active' class on the menu
+ * and the toggler button.
+ */
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('active');
+  menu.classList.toggle('active');
+})
